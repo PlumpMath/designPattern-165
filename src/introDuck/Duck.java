a7a57137af2e84038459cea5bbde7693cce74258
Mark Kelvin Pineda <mpineda@exist.com>
2016-06-13 16:02:15 +0800

    Initial commit for Head First's Design Pattern

---
+++ src/introDuck/Duck.java
@@ -0,0 +1,64 @@
+package introDuck;
+
+public class Duck {
+	
+	private int id;
+	private String duckType;
+	private String duckDescription;
+	private boolean isRealDuck;
+	
+	
+	
+	public Duck() {
+		super();
+	}
+	
+	public Duck(int id, String duckType, String duckDescription, boolean isRealDuck) {
+		super();
+		this.id = id;
+		this.duckType = duckType;
+		this.duckDescription = duckDescription;
+		this.isRealDuck = isRealDuck;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDuckType() {
+		return duckType;
+	}
+	public void setDuckType(String duckType) {
+		this.duckType = duckType;
+	}
+	public String getDuckDescription() {
+		return duckDescription;
+	}
+	public void setDuckDescription(String duckDescription) {
+		this.duckDescription = duckDescription;
+	}
+	public boolean isRealDuck() {
+		return isRealDuck;
+	}
+	public void setRealDuck(boolean isRealDuck) {
+		this.isRealDuck = isRealDuck;
+	}
+	
+	public void quack(){
+		System.out.println("Quack Quack");
+	}
+	
+	public void swim(){
+		System.out.println("//The duck is swimming !");
+	}
+	
+	public void fly(){
+		System.out.println("//The duck is flying !");
+	}
+	
+	public void display(){
+		System.out.println("Duck Name is " + this.getId());
+	}
+
+}
